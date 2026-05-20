9dc246b3df30d7552ae3d0d771e84e5db88a1764
Grim4 <158762011+Grim4@users.noreply.github.com>
2026-05-20 16:17:01 +0100

    Added distance estimation

--- include/Motors_simple.ino
+++ include/Motors_simple.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <Motoron.h>
+#include <math.h>
 
 MotoronI2C mc;
 
@@ -8,8 +9,8 @@ const uint8_t MOTOR_LEFT  = 1;
 const uint8_t MOTOR_RIGHT = 2;
 
 // Max Motoron speed is usually -800 to +800
-const int16_t MAX_SPEED_R = 800*0.96;
-const int16_t MAX_SPEED_L = -800;
+const int16_t MAX_SPEED_R = 800 * (1.4264/1.4681);
+const int16_t MAX_SPEED_L = 800;
 
 // Same acceleration/deceleration for both motors
 const uint16_t ACCEL = 800;
@@ -17,10 +18,69 @@ const uint16_t DECEL = 800;
 
 unsigned long lastPrintTime = 0;
 
+const uint8_t ENC_LEFT_A  = 28;
+const uint8_t ENC_LEFT_B  = 26;
+const uint8_t ENC_RIGHT_A = 22;
+const uint8_t ENC_RIGHT_B = 24;
+
+const long wheel_diameter = 38.35;
+
+volatile long encoderLeftCount = 0;
+volatile long encoderRightCount = 0;
+
 void checkpoint(const char *message)
 {
-  Serial.println(message);
-  Serial.flush();   // Makes sure the message is sent before continuing
+  //Serial.println(message);
+  //Serial.flush();   // Makes sure the message is sent before continuing
+}
+
+void readLeftEncoder()
+{
+  if (digitalRead(ENC_LEFT_A) == digitalRead(ENC_LEFT_B)) {
+    encoderLeftCount--;
+  } else {
+    encoderLeftCount++;
+  }
+}
+
+void readRightEncoder()
+{
+  if (digitalRead(ENC_RIGHT_A) == digitalRead(ENC_RIGHT_B)) {
+    encoderRightCount++;
+  } else {
+    encoderRightCount--;
+  }
+}
+
+long getLeftEncoder()
+{
+  noInterrupts();
+  long value = encoderLeftCount;
+  interrupts();
+  return value;
+}
+
+long getRightEncoder()
+{
+  noInterrupts();
+  long value = encoderRightCount;
+  interrupts();
+  return value;
+}
+
+void printEncoders()
+{
+  Serial.print("Left encoder: ");
+  Serial.print(getLeftEncoder());
+  Serial.print(", ");
+  Serial.print(" | Right encoder: ");
+  Serial.println(getRightEncoder());
+  Serial.print("Error: ");
+  Serial.println(getLeftEncoder() - getRightEncoder());
+  Serial.print("Estimated distance traveled (L): ");
+  Serial.println((getLeftEncoder() * M_PI * wheel_diameter) / (12 * 100));
+  Serial.print("Estimated distance traveled (R): ");
+  Serial.println((getRightEncoder() * M_PI * wheel_diameter) / (12 * 100));
 }
 
 void setup()
@@ -95,26 +155,47 @@ void setup()
   checkpoint("27. Right motor speed set");
 
   checkpoint("28. Setup complete, motors should be running");
+
+  delay(100);
+
+  pinMode(ENC_LEFT_A, INPUT_PULLUP);
+  checkpoint("29. Pinmode Left A");
+  pinMode(ENC_LEFT_B, INPUT_PULLUP);
+  checkpoint("30. Pinmode Left B");
+  pinMode(ENC_RIGHT_A, INPUT_PULLUP);
+  checkpoint("31. Pinmode Right A");
+  pinMode(ENC_RIGHT_B, INPUT_PULLUP);
+  checkpoint("32. Pinmode Right B");
+
+  attachInterrupt(digitalPinToInterrupt(ENC_LEFT_A), readLeftEncoder, CHANGE);
+  checkpoint("33. Attach interrupt Left");
+  attachInterrupt(digitalPinToInterrupt(ENC_RIGHT_A), readRightEncoder, CHANGE);
+  checkpoint("34. Attach interrupt Right");
+  Serial.println("");
+  Serial.print("Done");
+  Serial.println("");
+  Serial.println("");
 }
 
 void loop()
 {
 
-  mc.setSpeed(MOTOR_LEFT, MAX_SPEED_L);
-  mc.setSpeed(MOTOR_RIGHT, -MAX_SPEED_R);
-  delay(750);
-  mc.setSpeed(MOTOR_LEFT, 0);
-  mc.setSpeed(MOTOR_RIGHT, 0);
-
-
-
-  if (millis() - lastPrintTime >= 1000) {
+    if (millis() - lastPrintTime >= 1000) {
     lastPrintTime = millis();
 
-    Serial.print("Loop running. millis = ");
-    Serial.println(millis());
+    //Serial.print("Loop running. millis = ");
+    Serial.print(millis());
+    Serial.print(" ,");
+    printEncoders();
+
     Serial.flush();
   }
 
-  delay(3000);
+  mc.setSpeed(MOTOR_LEFT, MAX_SPEED_L);
+  mc.setSpeed(MOTOR_RIGHT, MAX_SPEED_R);
+  delay(1000);
+  mc.setSpeed(MOTOR_LEFT, 0);
+  mc.setSpeed(MOTOR_RIGHT, 0);
+
+  delay(1000);
 }
